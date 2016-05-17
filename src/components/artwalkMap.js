b4c88f293178355ad7c80d58b8a1fbacc8860dba
Matthew D. Huff <matthewhuff89@gmail.com>
2016-05-16 22:36:29 -0400

    a user can press location and the map will go to them...only once though right now

--- src/components/artwalkMap.js
+++ src/components/artwalkMap.js
@@ -4,35 +4,65 @@ import {
   View,
   Text,
   StyleSheet,
-  MapView
+  MapView,
+  Image
 } from 'react-native';
+var Geolocation = require('Geolocation');
+
 
 var artists = require('../../artists.json');
-console.log(artists);
+artists = artists.map(function(artist){
+  artist.title = artist.firstName + " " + artist.lastName;
+  artist.subtitle = artist.address;
+  return artist;
+})
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
       pins: artists,
-      location: {
-        latitude: 40.6968804,
-        longitude: -73.9688704
-      }
+      position: {
+        coords: {
+          latitude: 40.6968804,
+          longitude: -73.9688704
+        }
+      },
+      lastPosition: 'unknown'
     };
   },
   render: function(){
     return (
       <View style={styles.container}>
-        <TouchableHighlight
-          style={styles.navigation}
-          onPress={this.onSponsorPress}
-          >
-          <Text>Sponsors</Text>
-        </TouchableHighlight>
+        <View style={styles.navigation}>
+          <TouchableHighlight
+            onPress={this.onLocationPress}
+            >
+            <Text
+              style={styles.text}
+              >
+              Location
+            </Text>
+          </TouchableHighlight>
+          <Image
+            source={require('../images/sonya-stroll-logo.jpg')}
+            style={styles.image}
+            resizeMode='contain'
+            />
+          <TouchableHighlight
+            onPress={this.onSponsorPress}
+            >
+            <Text
+              style={styles.text}
+              >
+              Sponsors
+            </Text>
+          </TouchableHighlight>
+        </View>
+
         <MapView
           annotations={this.state.pins}
           style={styles.map}
-          region={this.state.location}
+          region={this.state.position.coords}
           showsUserLocation={true}
           maxDelta={0.05}
           >
@@ -42,6 +72,16 @@ module.exports = React.createClass({
   },
   onSponsorPress: function() {
     this.props.navigator.push({name: 'sponsors'})
+  },
+  onLocationPress: function() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({position})
+        navigator.geolocation.clearWatch(this.watchID);
+      },
+      (error) => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    )
   }
 });
 
@@ -54,16 +94,25 @@ var styles = StyleSheet.create({
   },
   navigation: {
     flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: 10
   },
   map: {
-    flex: 5
+    flex: 6
   },
   textWrapper: {
 
   },
   text: {
-
+    color: 'blue',
+    fontSize: 18
+  },
+  image: {
+    marginLeft: 15,
+    marginRight: 15,
+    height: 75,
+    width: 150
   }
 });
